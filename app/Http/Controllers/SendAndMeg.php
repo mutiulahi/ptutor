e8509dc74d824eb04523596eb7780314c71c501e
mutiulahi <tesleemolamilekan902@gmail.com>
2022-02-08 13:59:20 +0100

    fixe message

--- app/Http/Controllers/SendAndMeg.php
+++ app/Http/Controllers/SendAndMeg.php
@@ -27,7 +27,8 @@ class SendAndMeg extends Controller
                             ->join('users', 'messages.destination', '=', 'users.id') 
                             ->select('users.*', 'messages.*') 
                             ->get();
-                            
+        $query = DB::table('messages')->select('*')->where('sender',$id);
+        $users = $query->addSelect('users')->where('id','messages.destination')->get();
 
         $status = DB::table('tutregisters')
                             ->select('user_id')
